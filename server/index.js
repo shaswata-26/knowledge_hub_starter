0615d5f3a965cb840e19a433c106960c8803e9ef
Shaswata Panda <106397517+shaswata-26@users.noreply.github.com>
2025-09-01 14:50:20 +0530

    update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // --- CORS Setup ---
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "https://knowledgehubmern.netlify.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
